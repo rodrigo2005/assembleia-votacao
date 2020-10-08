d16ebdabdd8824b51247e924a36d38aa04c82b61
Rodrigo Araújo <rodrigo.rsa2005@gmail.com>
2020-10-08 17:00:50 -0300

    testes

---
+++ src/test/java/br/com/sicredi/entrevista/assembleia/excecao/Constantes.java
@@ -0,0 +1,8 @@
+package br.com.sicredi.entrevista.assembleia.excecao;
+
+public class Constantes {
+
+    public static final String THE_FIELD_DESCRIPTION_IS_REQUIRED = "The field 'description' is required.";
+
+
+}
